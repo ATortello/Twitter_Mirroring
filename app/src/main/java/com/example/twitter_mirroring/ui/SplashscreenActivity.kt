f844202c7664edc43cd710ea701fd5e7411c9edc
Agustin R Tortello D <arafaelt01@gmail.com>
2022-06-14 21:40:34 -0500

    Inicio de proyecto

---
+++ app/src/main/java/com/example/twitter_mirroring/ui/SplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.twitter_mirroring.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.twitter_mirroring.R
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
