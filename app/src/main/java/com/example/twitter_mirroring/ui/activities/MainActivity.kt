b4d33344e5fa35a252c9b13e79aa9ddc3349b489
Agustin R Tortello D <arafaelt01@gmail.com>
2022-06-16 13:02:37 -0500

    Adición de archivos XML para pantallas del reto

--- app/src/main/java/com/example/twitter_mirroring/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/twitter_mirroring/ui/activities/MainActivity.kt
@@ -1,12 +1,12 @@
-package com.example.twitter_mirroring.ui
+package com.example.twitter_mirroring.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.twitter_mirroring.R
 
-class SplashscreenActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splashscreen)
+        setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/example/twitter_mirroring/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,41 @@
+package com.example.twitter_mirroring.ui.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.example.twitter_mirroring.R
+
+class SplashscreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        //Adding animation to Twitter's Logo when starting app
+        val onStartAppAnimation = AnimationUtils.loadAnimation(this, R.anim.animation)
+        ivLogoTwitter.startAnimation(onStartAppAnimation)
+
+        //Adding action when animation ends to sent user to Main Activity screen
+        val intent = Intent(this, MainActivity::class.java)
+
+        //Implementing functions depending on animation's behavior
+        onStartAppAnimation.setAnimationListener(object : Animation.AnimationListener
+        {
+            //Nothing will be implemented here yet
+            override fun onAnimationStart(animation: Animation?){}
+
+            //When animation ends, send user to Main activity and close current activity
+            override fun onAnimationEnd(animation: Animation?)
+            {
+                startActivity(intent)
+                finish()
+            }
+
+            //Nothing will be implemented here yet
+            override fun onAnimationRepeat(animation: Animation?){}
+        })
+    }
+}
