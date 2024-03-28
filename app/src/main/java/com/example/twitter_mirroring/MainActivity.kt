d89e1385d973c6cd16eda1bb54287233acecad9e
Agustin R Tortello D <arafaelt01@gmail.com>
2024-03-28 14:05:58 -0500

    003. Applying YouTube tutorial for custom icons in Bottom NavBar

--- app/src/main/java/com/example/twitter_mirroring/MainActivity.kt
+++ app/src/main/java/com/example/twitter_mirroring/MainActivity.kt
@@ -2,26 +2,56 @@ package com.example.twitter_mirroring
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
+import com.example.twitter_mirroring.databinding.ActivityMainBinding
+import com.example.twitter_mirroring.view.ui.fragments.HomeFragment
+import com.example.twitter_mirroring.view.ui.fragments.MessagesFragment
+import com.example.twitter_mirroring.view.ui.fragments.NotificationsFragment
+import com.example.twitter_mirroring.view.ui.fragments.SearchFragment
+import com.example.twitter_mirroring.view.ui.fragments.SpacesFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+     //Implementing ViewBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-//        //Set a new AndroidX Toolbar
-//        setSupportActionBar(findViewById(R.id.toolbarMain))
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //Setting up the navigation for the app
-        configNav()
+//        configNav()
+        
+        val homeFragment = HomeFragment()
+        val searchFragment = SearchFragment()
+        val communitiesFragment = SpacesFragment()
+        val notificationFragment = NotificationsFragment()
+        val messageFragment = MessagesFragment()
+        
+        binding.bnvMenu.setOnNavigationItemSelectedListener { 
+            when(it.itemId) {
+                R.id.navHomeFragment -> { setCurrentFragment(homeFragment) }
+                R.id.navSearchFragment -> { setCurrentFragment(searchFragment) }
+                R.id.navCommunitiesFragment -> { setCurrentFragment(communitiesFragment) }
+                R.id.navNotificationsFragment -> { setCurrentFragment(notificationFragment) }
+                R.id.navDirectMessagesFragment -> { setCurrentFragment(messageFragment) }
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainer, fragment)
+            commit()
     }
 
     //Method to indicate the bottom navigation menu and the navigation controller
-    fun configNav() {
-        NavigationUI.setupWithNavController(bnvMenu, Navigation.findNavController(this, R.id.fragmentContainer))
-    }
+//    fun configNav() {
+//        NavigationUI.setupWithNavController(bnvMenu, Navigation.findNavController(this, R.id.fragmentContainer))
+//    }
 }
--- app/src/main/java/com/example/twitter_mirroring/view/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/twitter_mirroring/view/ui/fragments/HomeFragment.kt
@@ -5,24 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.twitter_mirroring.R
+import com.example.twitter_mirroring.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(){
+
+    //Implementing ViewBinding
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) { super.onCreate(savedInstanceState) }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-
-//        root.findViewById<MenuItem>(R.id.navHomeFragment)
-
-        // Return the inflate layout for this fragment
-        return root
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?)
     {
         super.onViewCreated(view, savedInstanceState)
+        binding.ivProfilePicture.setOnClickListener { Toast.makeText(activity, "Profile picture clicked!", Toast.LENGTH_SHORT).show() }
+
     }
 }
