545e3437bfe7452f11c810f4dfba2f9d0c168002
Agustin R Tortello D <arafaelt01@gmail.com>
2024-03-28 14:12:13 -0500

    004. Adjusting and restoring MainActivity code for Bottom NavBar

--- app/src/main/java/com/example/twitter_mirroring/MainActivity.kt
+++ app/src/main/java/com/example/twitter_mirroring/MainActivity.kt
@@ -2,15 +2,9 @@ package com.example.twitter_mirroring
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.example.twitter_mirroring.databinding.ActivityMainBinding
-import com.example.twitter_mirroring.view.ui.fragments.HomeFragment
-import com.example.twitter_mirroring.view.ui.fragments.MessagesFragment
-import com.example.twitter_mirroring.view.ui.fragments.NotificationsFragment
-import com.example.twitter_mirroring.view.ui.fragments.SearchFragment
-import com.example.twitter_mirroring.view.ui.fragments.SpacesFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -24,34 +18,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         //Setting up the navigation for the app
-//        configNav()
-        
-        val homeFragment = HomeFragment()
-        val searchFragment = SearchFragment()
-        val communitiesFragment = SpacesFragment()
-        val notificationFragment = NotificationsFragment()
-        val messageFragment = MessagesFragment()
-        
-        binding.bnvMenu.setOnNavigationItemSelectedListener { 
-            when(it.itemId) {
-                R.id.navHomeFragment -> { setCurrentFragment(homeFragment) }
-                R.id.navSearchFragment -> { setCurrentFragment(searchFragment) }
-                R.id.navCommunitiesFragment -> { setCurrentFragment(communitiesFragment) }
-                R.id.navNotificationsFragment -> { setCurrentFragment(notificationFragment) }
-                R.id.navDirectMessagesFragment -> { setCurrentFragment(messageFragment) }
-            }
-            true
-        }
-    }
-
-    private fun setCurrentFragment(fragment: Fragment) =
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fragmentContainer, fragment)
-            commit()
+        configNav()
     }
 
     //Method to indicate the bottom navigation menu and the navigation controller
-//    fun configNav() {
-//        NavigationUI.setupWithNavController(bnvMenu, Navigation.findNavController(this, R.id.fragmentContainer))
-//    }
+    fun configNav() {
+        NavigationUI.setupWithNavController(bnvMenu, Navigation.findNavController(this, R.id.fragmentContainer))
+    }
 }
