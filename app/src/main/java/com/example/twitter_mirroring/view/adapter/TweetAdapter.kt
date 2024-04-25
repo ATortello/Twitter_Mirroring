5d81d8b4a346a23fefb765667efc323def6d6326
Agustin R Tortello D <arafaelt01@gmail.com>
2024-04-25 06:48:19 -0500

    015. Fixing (4/4) UI detected issues on user's info. section - Part 2

--- app/src/main/java/com/example/twitter_mirroring/view/adapter/TweetAdapter.kt
+++ app/src/main/java/com/example/twitter_mirroring/view/adapter/TweetAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.twitter_mirroring.view.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,7 @@ import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.RequestOptions.noTransformation
 import com.example.twitter_mirroring.R
 import com.example.twitter_mirroring.model.TweetData
 import java.text.SimpleDateFormat
@@ -33,65 +35,85 @@ class TweetAdapter(val tweetListener: TweetListener) : RecyclerView.Adapter<Twee
     //With the this class the frontend and the logic are connected
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     {
-        var ivProfilePictureFeed: ImageView? = itemView.findViewById(R.id.ivProfilePictureFeed)
-        var ivVerifiedLogoFeed: ImageView? = itemView.findViewById(R.id.ivVerifiedLogoFeed)
-        var photo1: ImageView? = itemView.findViewById(R.id.ivTopLeftImageFeed)
-        var photo2: ImageView? = itemView.findViewById(R.id.ivTopRightImageFeed)
-        var photo3: ImageView? = itemView.findViewById(R.id.ivBottomLeftImageFeed)
-        var photo4: ImageView? = itemView.findViewById(R.id.ivBottomRightImageFeed)
-        var tvCantLikesFeed: TextView? = itemView.findViewById(R.id.tvCantLikesFeed)
-        var tvCantRepliesFeed: TextView? = itemView.findViewById(R.id.tvCantRepliesFeed)
-        var tvCantRetweetsFeed: TextView? = itemView.findViewById(R.id.tvCantRetweetsFeed)
-        var tvCantViewsFeed: TextView? = itemView.findViewById(R.id.tvCantViewsFeed)
-        var tvPublicNameFeed: TextView? = itemView.findViewById(R.id.tvPublicNameFeed)
-        var tvRealUsernameFeed: TextView? = itemView.findViewById(R.id.tvRealUsernameFeed)
-        var tvTimePostedFeed: TextView? = itemView.findViewById(R.id.tvTimePostedFeed)
-        var tvTweetContentFeed: TextView? = itemView.findViewById(R.id.tvTweetContentFeed)
-        var cvMediaContent: CardView? = itemView.findViewById(R.id.cvMediaFeed)
-        var llMediaFeed: LinearLayout? = itemView.findViewById(R.id.llMediaFeed)
+        val ivProfilePictureFeed: ImageView? = itemView.findViewById(R.id.ivProfilePictureFeed)
+        val ivVerifiedLogoFeed: ImageView? = itemView.findViewById(R.id.ivVerifiedLogoFeed)
+        val photo1: ImageView? = itemView.findViewById(R.id.ivTopLeftImageFeed)
+        val photo2: ImageView? = itemView.findViewById(R.id.ivTopRightImageFeed)
+        val photo3: ImageView? = itemView.findViewById(R.id.ivBottomLeftImageFeed)
+        val photo4: ImageView? = itemView.findViewById(R.id.ivBottomRightImageFeed)
+        val tvCantLikesFeed: TextView? = itemView.findViewById(R.id.tvCantLikesFeed)
+        val tvCantRepliesFeed: TextView? = itemView.findViewById(R.id.tvCantRepliesFeed)
+        val tvCantRetweetsFeed: TextView? = itemView.findViewById(R.id.tvCantRetweetsFeed)
+        val tvCantViewsFeed: TextView? = itemView.findViewById(R.id.tvCantViewsFeed)
+        val tvPublicNameFeed: TextView? = itemView.findViewById(R.id.tvPublicNameFeed)
+        val tvRealUsernameFeed: TextView? = itemView.findViewById(R.id.tvRealUsernameFeed)
+        val tvTimePostedFeed: TextView? = itemView.findViewById(R.id.tvTimePostedFeed)
+        val tvTweetContentFeed: TextView? = itemView.findViewById(R.id.tvTweetContentFeed)
+        val cvMediaContent: CardView? = itemView.findViewById(R.id.cvMediaFeed)
+        val llMediaFeed: LinearLayout? = itemView.findViewById(R.id.llMediaFeed)
+
+        init {
+            cvMediaContent!!.visibility = View.GONE
+            llMediaFeed!!.visibility = View.GONE
+            photo1!!.visibility = View.GONE
+            photo2!!.visibility = View.GONE
+            photo3!!.visibility = View.GONE
+            photo4!!.visibility = View.GONE
+        }
     }
 
     //Here the data obtained from the source is matched with the frontend components
     override fun onBindViewHolder(p0: TweetAdapter.ViewHolder, p1: Int) {
         val tweet = listOfTweets[p1]
+        val visibility = View.VISIBLE
+        val context = p0.itemView.context
+        val circleTransformation = RequestOptions.circleCropTransform()
+        val noTransformation = noTransformation()
 
         /*Setting images: Profile, verified logo (if applies) and media content*/
-        Glide
-            .with(p0.itemView.context)
-            .load(tweet.profilePhoto)
-            .apply(RequestOptions.circleCropTransform())
-            .into(p0.ivProfilePictureFeed!!)
+        showWithGlide(context, tweet.profilePhoto, circleTransformation, p0.ivProfilePictureFeed)
 
-        if(tweet.verifiedLogo) { p0.ivVerifiedLogoFeed!!.visibility = View.VISIBLE }
-        else { p0.ivVerifiedLogoFeed!!.visibility = View.GONE }
+        //Showing logo or not
+        if(tweet.verifiedLogo) p0.ivVerifiedLogoFeed!!.visibility = View.VISIBLE
+        else p0.ivVerifiedLogoFeed!!.visibility = View.GONE
 
-        //Managing CardView grid to show images
-        when(tweet.mediaCount) {
-            1 -> { p0.cvMediaContent!!.visibility = View.VISIBLE
-                Glide.with(p0.itemView.context).load(tweet.photoUrl1).into(p0.photo1!!) }
+        //Managing CardView elements to show images get from Firestore
+        if (!tweet.hasMedia) p0.cvMediaContent!!.visibility = View.GONE
+        else {  p0.cvMediaContent!!.visibility = visibility
+            p0.photo1!!.visibility = visibility
 
-            2 -> { p0.cvMediaContent!!.visibility = View.VISIBLE
-                p0.llMediaFeed!!.visibility = View.VISIBLE
-                Glide.with(p0.itemView.context).load(tweet.photoUrl1).into(p0.photo1!!)
-                Glide.with(p0.itemView.context).load(tweet.photoUrl2).into(p0.photo2!!) }
+//            if(tweet.photoUrl1.contains(".mp4")) {
+//                p0.photo1!!.visibility = View.VISIBLE
+//                Glide.with(p0.itemView.context).load(tweet.photoUrl1).into(p0.photo1!!)
+//            }
 
-            3 -> { p0.cvMediaContent!!.visibility = View.VISIBLE
-                p0.llMediaFeed!!.visibility = View.VISIBLE
-                p0.photo4!!.visibility = View.VISIBLE
-                Glide.with(p0.itemView.context).load(tweet.photoUrl1).into(p0.photo1!!)
-                Glide.with(p0.itemView.context).load(tweet.photoUrl2).into(p0.photo2!!)
-                Glide.with(p0.itemView.context).load(tweet.photoUrl3).into(p0.photo3!!) }
+            //Managing CardView grid to show images
+            when(tweet.mediaCount) {
+                1 -> showWithGlide(context, tweet.photoUrl1, noTransformation, p0.photo1)
 
-            4 -> { p0.cvMediaContent!!.visibility = View.VISIBLE
-                p0.llMediaFeed!!.visibility = View.VISIBLE
-                p0.photo3!!.visibility = View.VISIBLE
-                p0.photo4!!.visibility = View.VISIBLE
-                Glide.with(p0.itemView.context).load(tweet.photoUrl1).into(p0.photo1!!)
-                Glide.with(p0.itemView.context).load(tweet.photoUrl2).into(p0.photo2!!)
-                Glide.with(p0.itemView.context).load(tweet.photoUrl3).into(p0.photo3!!)
-                Glide.with(p0.itemView.context).load(tweet.photoUrl4).into(p0.photo4!!)
+                2 -> { p0.llMediaFeed!!.visibility = visibility
+                    p0.photo2!!.visibility = visibility
+                    showWithGlide(context, tweet.photoUrl1, noTransformation, p0.photo1)
+                    showWithGlide(context, tweet.photoUrl2, noTransformation, p0.photo2) }
+
+                3 -> { p0.llMediaFeed!!.visibility = visibility
+                    p0.photo2!!.visibility = visibility
+                    p0.photo4!!.visibility = visibility
+                    showWithGlide(context, tweet.photoUrl1, noTransformation, p0.photo1)
+                    showWithGlide(context, tweet.photoUrl2, noTransformation, p0.photo2)
+                    showWithGlide(context, tweet.photoUrl3, noTransformation, p0.photo4) }
+
+                4 -> { p0.llMediaFeed!!.visibility = visibility
+                    p0.photo2!!.visibility = visibility
+                    p0.photo3!!.visibility = visibility
+                    p0.photo4!!.visibility = visibility
+                    showWithGlide(context, tweet.photoUrl1, noTransformation, p0.photo1)
+                    showWithGlide(context, tweet.photoUrl2, noTransformation, p0.photo2)
+                    showWithGlide(context, tweet.photoUrl3, noTransformation, p0.photo3)
+                    showWithGlide(context, tweet.photoUrl4, noTransformation, p0.photo4) }
+
+                else -> p0.cvMediaContent.visibility = View.GONE
             }
-            else -> p0.cvMediaContent!!.visibility = View.GONE
         }
 
         /*Setting texts:*/
@@ -108,8 +130,6 @@ class TweetAdapter(val tweetListener: TweetListener) : RecyclerView.Adapter<Twee
         val cal = Calendar.getInstance()
         cal.time = tweet.hourAndDate
 
-        /*var diffInDays = (diffInSeconds / 86400).toInt()
-        println("Log ARTD - Difference: ${diffInDays} days")*/
         //Setting date formats according to how long the tweets were posted
         val pattern = when(diffInSeconds) {
             in 0..59 -> "s"
@@ -119,7 +139,6 @@ class TweetAdapter(val tweetListener: TweetListener) : RecyclerView.Adapter<Twee
             in 604800..2678399 -> "d MMM"
             else -> "d MMM yy"
         }
-        /*println("Log ARTD - Pattern selected: $pattern")*/
 
         //Setting texts to show depending the date's format previously configured
         val simpleDateFormat = SimpleDateFormat(pattern)
@@ -131,23 +150,24 @@ class TweetAdapter(val tweetListener: TweetListener) : RecyclerView.Adapter<Twee
             "d MMM" -> simpleDateFormat.format(cal.time)
             else -> simpleDateFormat.format(cal.time)
         }
-        /*println("Log ARTD - Time to display: $timeToDisplay")*/
-
         p0.tvTimePostedFeed!!.setText(" · $timeToDisplay")
-        //Format for detailed tweet page
-        //val simpleDateFormatDetailed = SimpleDateFormat("hh:mm a '·' MMM d, yyyy")
 
         //On Click event
         p0.itemView.setOnClickListener { tweetListener.onTweetClicked(tweet, p1) }
+    }
 
+    fun showWithGlide(context: Context, url: String, requestOptions: RequestOptions = noTransformation(), imageView: ImageView?) {
+        Glide.with(context)
+             .load(url)
+             .apply(requestOptions)
+             .into(imageView!!)
     }
 
     //Difference between the current date and the post's dates from Firestore
     fun timeGap(firestoreDate: Date, simpleDateFormat: SimpleDateFormat) : Int =
         (simpleDateFormat.format(System.currentTimeMillis()).toInt()) - (simpleDateFormat.format(firestoreDate).toInt())
 
-    fun updateData(data: List<TweetData>)
-    {
+    fun updateData(data: List<TweetData>) {
         listOfTweets.clear()
         listOfTweets.addAll(data)
         notifyDataSetChanged()
